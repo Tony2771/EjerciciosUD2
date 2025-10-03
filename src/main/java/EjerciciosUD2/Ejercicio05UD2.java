dd76a0bc67d3cfd9bf271b4c4960cacc45f695c9
Antonio <condevarelaantonio@gmail.com>
2025-10-03 09:52:29 +0200

    EjerciciosUD2

--- src/main/java/EjerciciosUD2/Ejercicio05UD2.java
+++ src/main/java/EjerciciosUD2/Ejercicio05UD2.java
@@ -1,9 +1,9 @@
 package EjerciciosUD2;
 
 
-public class EjerciciosUD2 {
+public class Ejercicio05UD2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
